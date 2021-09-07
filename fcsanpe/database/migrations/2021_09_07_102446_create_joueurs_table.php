1d54ec464fea9ab77a603349fe19c3bb7ec706f4
gnakale <gnakaleroland@gmail.com>
2021-09-07 10:50:23 +0000

    creation du joueurs

---
+++ fcsanpe/database/migrations/2021_09_07_102446_create_joueurs_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJoueursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('joueurs', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->date('datenais');
+            $table->string('lieunais');
+            $table->string('taille');
+            $table->string('poids');
+            $table->string('numéro_maillot');
+            $table->string('position_terrain');
+            $table->text('details_joueurs');
+            $table->string('photo_joueurs');
+            $table->string('vidéo_joueurs');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('joueurs');
+    }
+}
